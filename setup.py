dd287cadb52606e11dfc06b3eee2d69a926e2179
Theron Luhn <theron@luhn.com>
2020-07-27 08:59:27 -0700

    Github Actions.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 VERSION = '0.1.0'
 
 INSTALL_REQUIRES = [
-    'dataclasses;python_version<3.7',
+    'dataclasses; python_version<"3.7"',
 ]
 
 EXTRAS_REQUIRE = {
