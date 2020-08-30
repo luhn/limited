94e6a327e29e02cc5ed3343ff7cf5382b182f277
Theron Luhn <theron@luhn.com>
2020-08-29 18:58:34 -0700

    Add integration tests.

---
+++ tests/test_backends/test_integration.py
@@ -0,0 +1,24 @@
+import secrets
+from time import sleep
+
+import pytest
+
+from limited.backend.memory import MemoryBackend
+
+
+@pytest.fixture(scope='session')
+def memory_backend():
+    return MemoryBackend(100)
+
+
+@pytest.mark.parametrize('backend', [
+    pytest.lazy_fixture('memory_backend'),
+])
+def test_integration(backend):
+    zone = backend('mybackend', 1.0, 10)
+    identity = secrets.token_urlsafe()
+    assert zone.count(identity) == 10.0
+    assert zone.remove(identity, 5)
+    assert 4.5 < zone.count(identity) < 5.5
+    sleep(1.0)
+    assert 5.5 < zone.count(identity) < 6.5
